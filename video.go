42cec4e14c1dc1e261f2de6e467b5142fc2af157
Pietro Gagliardi <pietro10@mac.com>
2013-09-03 15:39:53 -0400

    Made tiles transparent so I can overlay three layers.

--- video.go
+++ video.go
@@ -43,8 +43,12 @@ func RenderTile(mapping uint16, palette color.Palette) image.Image {
 			left := b & 0xF
 			right := (b & 0xF0) >> 4
 			// TODO access data directly to save time?
-			tile.Set(x, y, palette[paletteLine + uint16(left)])
-			tile.Set(x + 1, y, palette[paletteLine + uint16(right)])
+			if left != 0 {			// transparent
+				tile.Set(x, y, palette[paletteLine + uint16(left)])
+			}
+			if right != 0 {
+				tile.Set(x + 1, y, palette[paletteLine + uint16(right)])
+			}
 		}
 	}
 
