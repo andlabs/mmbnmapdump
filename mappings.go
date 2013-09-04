6d3a2eeec01ab36736907c08402216216606477c
Pietro Gagliardi <pietro10@mac.com>
2013-09-04 04:19:08 -0400

    reworked the mappings structure to process the header within the loading function and provide a simpler interface to the outside. Also added documentation on the format to the file.

--- mappings.go
+++ mappings.go
@@ -6,20 +6,44 @@ import (
 )
 
 // the mappings format is simple
-// 0x10 bytes which are copied raw (the first two bytes of whicih appear to be extra special?)
+// a 0x10-byte header of the form
+// 	struct Header {
+// 		Width	byte
+// 		Height	byte
+// 		Unknown	[2]byte
+// 		BG0Off	uint32	// little endian
+// 		BG1Off	uint32	// little endian
+// 		BG2Off	uint32	// little endian
+// 	}
 // immediately followed by the mappings themselves, LZ77 compressed
+// When loaded, the header is first copied over untouched, then the compressed data is decompressed immediately afterward. The three offset fields are offset from the first byte of this block (so from the Width byte in the copied header).
 
 type Mappings struct {
-	Header	[0x10]byte
+	Width	byte
+	Height	byte
+	BGOff	[3]uint32
 	Data		[]byte
 }
 
 func ReadMappings(r io.Reader) (m *Mappings, err error) {
 	m = new(Mappings)
 
-	_, err = r.Read(m.Header[:])
+	var header [0x10]byte
+
+	_, err = r.Read(header[:])
 	if err != nil {
 		return nil, err		// TODO wrap with more text?
+	}
+
+	m.Width = header[0]
+	m.Height = header[1]
+	for i := 0; i < 3; i++ {
+		j := (i + 1) * 4
+		m.BGOff[i] = uint32(header[j]) |
+			(uint32(header[j + 1]) << 8) |
+			(uint32(header[j + 2]) << 16) |
+			(uint32(header[j + 3]) << 24)
+		m.BGOff[i] -= 0x10	// value in header is relative to top of header; make it relative to top of Data instead
 	}
 
 	m.Data, err = LZ77Decomp(r)
--- render.go
+++ render.go
@@ -8,17 +8,13 @@ import (
 )
 
 func Render(offsets [3]uint32, m *Mappings, palette color.Palette) image.Image {
-	width := int(uint32(m.Header[0]) * 8)
-	height := int(uint32(m.Header[1]) * 8)
+	width := int(uint32(m.Width) * 8)
+	height := int(uint32(m.Height) * 8)
 
 	img := image.NewNRGBA(image.Rect(0, 0, width, height))
 
 	for i := 0; i < 3; i++ {
-		dataoff := uint32(m.Header[(i + 1) * 4]) |
-			(uint32(m.Header[((i + 1) * 4) + 1]) << 8) |
-			(uint32(m.Header[((i + 1) * 4) + 2]) << 16) |
-			(uint32(m.Header[((i + 1) * 4) + 3]) << 24)
-		dataoff -= 0x10	// offset is stored relative to top of mappings; make it relative to top of m.Data instead
+		dataoff := m.BGOff[i]
 		mp := uint32(0)
 		for y := 0; y < height; y += 8 {
 			for x := 0; x < width; x += 8 {
